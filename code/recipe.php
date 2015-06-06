d2a79ff40718184c57c99a76c9ec37c17199ac24
Shun Nishitsuji <nishitsuji_shun@liginc.co.jp>
2015-06-06 11:46:21 +0900

    ファイル読み込み機能を追加

--- code/recipe.php
+++ code/recipe.php
@@ -1,3 +1,10 @@
 <?php
-    // レシピのタイトルを表示
-    echo "オムライス\n";
+    $file = null;
+
+    // ファイルを読み込んで出力
+    if (($file = fopen ( "code/recipe-data.txt", "r" ))) {
+        while (!feof($file)) {
+            echo fgets($file, 1024);
+        }
+    }
+    fclose($file);
