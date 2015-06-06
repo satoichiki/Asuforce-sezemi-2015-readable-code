8ba1c242cb7e3c4814298a7e74028861b6a2e082
佐藤一輝 <i.tennis0210@gmail.com>
2015-06-06 15:31:15 +0900

    仕様6をクラス化

--- code/recipe.php
+++ code/recipe.php
@@ -1,16 +1,18 @@
 <?php
+    $recipe = new Recipe;
+
     $file = fopen ($argv[1],"r");
 
     $recipe_id = $argv[2]; //ID指定の場合は数値を代入.それ以外はnull
 
     // ファイルを読み込んで出力
     if($file){
-        $recipe_data = readRecipe($file);
+        $recipe_data = $recipe->readFile($file);
         if($recipe_id){
             $id = $recipe_id-1;
             echo $recipe_id.": ".$recipe_data[$id];
         }else{
-            recipeGetAll($recipe_data);
+            $recipe->fetchAll($recipe_data);
         }
     }else{
         // 読み込み失敗時
@@ -19,20 +21,21 @@
 
     fclose($file);
 
-    //ファイルのデータを読み込み配列で返す
-    function readRecipe($file) {
-        $recipe_array = [];
-        //ファイルを1行ずつ読み込む
-        while($line = fgets($file)){
-            $recipe_array[] = $line;
+    Class Recipe {
+        public function readFile($file) {
+            $recipe_array = [];
+            //ファイルのデータを読み込み配列で返す
+            while($line = fgets($file)){
+                $recipe_array[] = $line;
+            }
+            return $recipe_array;
         }
-        return $recipe_array;
-    }
 
-    //レシピのデータを全て出力する
-    function recipeGetAll($recipe_array) {
-        for ($i = 0; $i < count($recipe_array); $i++){
-            $id = $i+1;
-            echo $id.": ".$recipe_array[$i];
+        //レシピのデータを全て出力する
+        public function fetchAll($recipe_array) {
+            for ($i = 0; $i < count($recipe_array); $i++){
+                $id = $i+1;
+                echo $id.": ".$recipe_array[$i];
+            }
         }
     }
