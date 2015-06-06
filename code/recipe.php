9169466e20a73c4f819e578f2dd561dbab42a979
Shun Nishitsuji <nishitsuji_shun@liginc.co.jp>
2015-06-06 11:50:41 +0900

    recipe.phpにコメントを追加

--- code/recipe.php
+++ code/recipe.php
@@ -3,8 +3,13 @@
 
     // ファイルを読み込んで出力
     if (($file = fopen ( "code/recipe-data.txt", "r" ))) {
+        // 読み込み成功時
         while (!feof($file)) {
             echo fgets($file, 1024);
         }
+    } else {
+        // 読み込み失敗時
+        echo "読み取り失敗";
     }
+
     fclose($file);
